8b8d0ea3fd10578012fd39577b2e6f29c93ddea6
jonghyeons <whdgus906@gmail.com>
2023-06-19 16:38:58 +0900

    Add Throttle

---
+++ ch04/throttle.go
@@ -0,0 +1,43 @@
+package ch04
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Throttle(e Effector, max, refill uint, d time.Duration) Effector {
+	var tokens = max
+	var once sync.Once
+
+	return func(ctx context.Context) (string, error) {
+		if ctx.Err() != nil {
+			return "", ctx.Err()
+		}
+
+		once.Do(func() {
+			ticker := time.NewTicker(d)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+
+				case <-ticker.C:
+					t := tokens + refill
+					if t > max {
+						t = max
+					}
+					tokens = t
+				}
+			}
+		})
+
+		if tokens <= 0 {
+			return "", fmt.Errorf("too many calls")
+		}
+
+		tokens--
+		return e(ctx)
+	}
+}
