f68eea60df277a85a05cfebc4b50114496c06e1e
jonghyeons <whdgus906@gmail.com>
2023-06-20 13:47:58 +0900

    Add Mutex

--- ch05/core.go
+++ ch05/core.go
@@ -1,24 +1,37 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
-var store = make(map[string]string)
 var ErrorNoSuchKey = errors.New("no such key")
 
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
 func Put(key, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	if _, exists := store[key]; exists {
-		return store[key], nil
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
+
+	if !ok {
+		return "", ErrorNoSuchKey
 	}
-	return "", ErrorNoSuchKey
+	return value, nil
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	delete(store.m, key)
 	return nil
 }
